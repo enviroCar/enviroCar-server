9e0c30d877d06850ece6c5953ca8aa8fca171823
Christian Autermann <christian@autermann.org>
2013-04-19 05:06:03 +0200

    initial import

---
+++ lcs-server-webapp/src/main/java/de/ifgi/lcs/LCSServletContextListener.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (C) 2013  Christian Autermann, Jan Alexander Wirwahn,
+ *                     Arne De Wall, Dustin Demuth, Saqib Rasheed
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package de.ifgi.lcs;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.servlet.GuiceServletContextListener;
+import com.sun.jersey.guice.JerseyServletModule;
+import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
+
+import de.ifgi.lcs.server.core.Database;
+import de.ifgi.lcs.server.lcs.server.rest.Root;
+import de.ifgi.lcs.server.mongo.MongoDatabase;
+
+/**
+ * @author Christian Autermann <c.autermann@52north.org>
+ */
+public class LCSServletContextListener extends GuiceServletContextListener {
+
+    @Override
+    protected Injector getInjector() {
+        return Guice.createInjector(new JerseyServletModule() {
+            @Override
+            protected void configureServlets() {
+                /* TODO add other modules */
+                bind(Root.class);
+                bind(Database.class).to(MongoDatabase.class);
+                serve("/*").with(GuiceContainer.class);
+            }
+        });
+    }
+}
