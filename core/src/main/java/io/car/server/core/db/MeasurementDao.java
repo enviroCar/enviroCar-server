f506e93f243f8fcc639335f56137f46b615b645b
dewall <a.dewall@wwu.de>
2013-05-04 19:42:06 +0200

    license header update

--- core/src/main/java/io/car/server/core/db/MeasurementDao.java
+++ core/src/main/java/io/car/server/core/db/MeasurementDao.java
@@ -1,10 +1,27 @@
+/**
+ * Copyright (C) 2013  Christian Autermann, Jan Alexander Wirwahn,
+ *                     Arne De Wall, Dustin Demuth, Saqib Rasheed
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package io.car.server.core.db;
 
 import io.car.server.core.Measurement;
 import io.car.server.core.Measurements;
 import io.car.server.core.Track;
 
-import com.vividsolutions.jts.geom.Envelope;
+import com.vividsolutions.jts.geom.Geometry;
 
 /**
  * 
@@ -18,9 +35,10 @@ public interface MeasurementDao {
 	
 	Measurements getByPhenomenon(String string);
 	Measurements getByTrack(Track track);
-	Measurements getByBbox(Envelope bbox);
+	Measurements getByBbox(Geometry bbox);
 	Measurements getByBbox(double minx, double miny, double maxx, double maxy);
 	
 	Measurements getAll();
 	Measurements getAll(int limit);
+
 }
--- mongo/src/main/java/io/car/server/mongo/MongoMeasurementDao.java
+++ mongo/src/main/java/io/car/server/mongo/MongoMeasurementDao.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright (C) 2013  Christian Autermann, Jan Alexander Wirwahn,
+ *                     Arne De Wall, Dustin Demuth, Saqib Rasheed
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package io.car.server.mongo;
 
 import io.car.server.core.Measurement;
@@ -9,7 +26,8 @@ import com.github.jmkgreen.morphia.Datastore;
 import com.github.jmkgreen.morphia.dao.BasicDAO;
 import com.github.jmkgreen.morphia.query.Query;
 import com.google.inject.Inject;
-import com.vividsolutions.jts.geom.Envelope;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
 
 /**
  * 
@@ -53,16 +71,17 @@ public class MongoMeasurementDao extends BasicDAO<MongoMeasurement, String> impl
 	}
 
 	@Override
-	public Measurements getByBbox(Envelope bbox) {
-		// TODO Auto-generated method stub
+	public Measurements getByBbox(Geometry bbox) {
+		Coordinate[] coords = bbox.get
 		return null;
 	}
 
 	@Override
 	public Measurements getByBbox(double minx, double miny, double maxx,
 			double maxy) {
-		// TODO Auto-generated method stub
-		return null;
+		Query<MongoMeasurement> q = createQuery();
+		q.field(MongoMeasurement.LOCATION).within(minx, miny, maxx, maxy);
+		return fetch(q);
 	}
 
 	@Override
