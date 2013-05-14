9be1b6b02ef3bb10a400eb83ffe54429eb9ab010
Christian Autermann <christian@autermann.org>
2013-05-14 17:39:43 +0200

    added missing annotations

--- rest/src/main/java/io/car/server/rest/provider/SensorProvider.java
+++ rest/src/main/java/io/car/server/rest/provider/SensorProvider.java
@@ -1,43 +1,39 @@
 /**
- * Copyright (C) 2013  Christian Autermann, Jan Alexander Wirwahn,
- *                     Arne De Wall, Dustin Demuth, Saqib Rasheed
+ * Copyright (C) 2013 Christian Autermann, Jan Alexander Wirwahn, Arne De Wall, Dustin Demuth, Saqib Rasheed
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package io.car.server.rest.provider;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.ext.Provider;
 
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
-import com.google.inject.Inject;
-
-import io.car.server.core.EntityFactory;
 import io.car.server.core.entities.Sensor;
 import io.car.server.rest.MediaTypes;
 
 /**
  * @author Christian Autermann <c.autermann@52north.org>
  */
+@Provider
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class SensorProvider extends AbstractJsonEntityProvider<Sensor> {
-    private EntityFactory factory;
-
-    @Inject
-    public SensorProvider(EntityFactory factory) {
+    public SensorProvider() {
         super(Sensor.class, MediaTypes.SENSOR_TYPE, MediaTypes.SENSOR_CREATE_TYPE, MediaTypes.SENSOR_MODIFY_TYPE);
-        this.factory = factory;
     }
 
     @Override
