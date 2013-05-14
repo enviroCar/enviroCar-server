10ea35c2ce9937d8fdff03fbbba3612f0e38da30
Christian Autermann <christian@autermann.org>
2013-05-14 17:52:33 +0200

    log mapped exceptions

--- rest/src/main/java/io/car/server/rest/mapper/AbstractExceptionMapper.java
+++ rest/src/main/java/io/car/server/rest/mapper/AbstractExceptionMapper.java
@@ -22,10 +22,14 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author Christian Autermann <c.autermann@52north.org>
  */
 public class AbstractExceptionMapper<T extends Throwable> implements ExceptionMapper<T> {
+    private static final Logger log = LoggerFactory.getLogger(AbstractExceptionMapper.class);
     private final Status status;
 
     public AbstractExceptionMapper(Status status) {
@@ -34,6 +38,7 @@ public class AbstractExceptionMapper<T extends Throwable> implements ExceptionMa
 
     @Override
     public Response toResponse(T exception) {
+        log.info("Mapping exception", exception);
         return Response
                 .status(status)
                 .type(MediaType.TEXT_PLAIN)
