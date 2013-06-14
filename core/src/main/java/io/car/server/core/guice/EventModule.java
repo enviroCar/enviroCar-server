a05c35f859e4b0da22dbc172a49198bcbd355f26
Daniel Nüst <d.nuest@52north.org>
2013-06-14 14:19:41 +0200

    added EventModule skeleton

---
+++ core/src/main/java/io/car/server/core/guice/EventModule.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2013  Matthes Rieke, Daniel Nüst
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.car.server.core.guice;
+
+import com.google.inject.AbstractModule;
+
+/**
+ * 
+ * @author Daniel Nüst (d.nuest@52north.org)
+ *
+ */
+public class EventModule extends AbstractModule {
+
+	@Override
+	protected void configure() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
