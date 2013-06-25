2874d43510347b80b736aa4f6ea740e60191bab7
Christian Autermann <christian@autermann.org>
2013-06-25 15:12:40 +0200

    formatting

--- rest/src/main/java/io/car/server/rest/encoding/rdf/linker/PhenomenonDCTermsLinker.java
+++ rest/src/main/java/io/car/server/rest/encoding/rdf/linker/PhenomenonDCTermsLinker.java
@@ -17,25 +17,28 @@
  */
 package io.car.server.rest.encoding.rdf.linker;
 
+import javax.ws.rs.core.UriBuilder;
+
 import com.google.inject.Provider;
 import com.hp.hpl.jena.rdf.model.Model;
+
 import io.car.server.core.entities.Phenomenon;
 import io.car.server.rest.encoding.rdf.RDFLinker;
 import io.car.server.rest.resources.PhenomenonsResource;
 import io.car.server.rest.resources.RootResource;
 import io.car.server.rest.rights.AccessRights;
-import javax.ws.rs.core.UriBuilder;
 
 /**
  *
  * @author Jan Wirwahn
  */
 public class PhenomenonDCTermsLinker implements RDFLinker<Phenomenon> {
-
-    public static final String ODBL_URL = "http://opendatacommons.org/licenses/odbl/";
+    public static final String ODBL_URL =
+            "http://opendatacommons.org/licenses/odbl/";
 
     @Override
-    public void link(Model m, Phenomenon t, AccessRights rights, Provider<UriBuilder> uriBuilder) {
+    public void link(Model m, Phenomenon t, AccessRights rights,
+                     Provider<UriBuilder> uriBuilder) {
         String uri = uriBuilder.get()
                 .path(RootResource.class)
                 .path(RootResource.PHENOMENONS)
