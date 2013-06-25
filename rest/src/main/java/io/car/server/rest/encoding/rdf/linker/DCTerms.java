9d9abc2b743e477eeb49819d4525810986906589
Christian Autermann <christian@autermann.org>
2013-06-25 15:10:23 +0200

    formatting

--- rest/src/main/java/io/car/server/rest/encoding/rdf/linker/DCTerms.java
+++ rest/src/main/java/io/car/server/rest/encoding/rdf/linker/DCTerms.java
@@ -10,13 +10,14 @@ import com.hp.hpl.jena.rdf.model.Property;
 
 /**
  *
- * @author jan
+ * @author Jan Wirwahn
  */
 public class DCTerms {
-    public static final String URI ="http://purl.org/dc/elements/1.1/";
-    public static final String PREFIX ="dcterms";
+    public static final String URI = "http://purl.org/dc/elements/1.1/";
+    public static final String PREFIX = "dcterms";
     private static final Model m = ModelFactory.createDefaultModel();
     public static final Property rights = m.createProperty(URI, "rights");
-            
-            
+
+    private DCTerms() {
+    }
 }
--- rest/src/main/java/io/car/server/rest/encoding/rdf/linker/UserDCTermsLinker.java
+++ rest/src/main/java/io/car/server/rest/encoding/rdf/linker/UserDCTermsLinker.java
@@ -1,24 +1,26 @@
 package io.car.server.rest.encoding.rdf.linker;
 
+import javax.ws.rs.core.UriBuilder;
+
 import com.google.inject.Provider;
 import com.hp.hpl.jena.rdf.model.Model;
+
 import io.car.server.core.entities.User;
 import io.car.server.rest.encoding.rdf.RDFLinker;
 import io.car.server.rest.resources.RootResource;
 import io.car.server.rest.resources.UsersResource;
 import io.car.server.rest.rights.AccessRights;
-import javax.ws.rs.core.UriBuilder;
 
 /**
- *
  * @author jan
  */
 public class UserDCTermsLinker implements RDFLinker<User> {
-
-    public static final String ODBL_URL = "http://opendatacommons.org/licenses/odbl/";
+    public static final String ODBL_URL =
+            "http://opendatacommons.org/licenses/odbl/";
 
     @Override
-    public void link(Model m, User t, AccessRights rights, Provider<UriBuilder> uriBuilder) {
+    public void link(Model m, User t, AccessRights rights,
+                     Provider<UriBuilder> uriBuilder) {
         m.setNsPrefix(DCTerms.PREFIX, DCTerms.URI);
         String uri = uriBuilder.get()
                 .path(RootResource.class)
