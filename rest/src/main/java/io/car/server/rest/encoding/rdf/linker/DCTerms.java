57ca1143a51a10cb2d3c27706799762eae9da620
Jan Wirwahn <jan.wirwahn@wwu.de>
2013-06-25 14:47:40 +0200

    Added rights property of Dublin-Core to User, Sensor, and Track.

---
+++ rest/src/main/java/io/car/server/rest/encoding/rdf/linker/DCTerms.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.car.server.rest.encoding.rdf.linker;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
+
+/**
+ *
+ * @author jan
+ */
+public class DCTerms {
+    public static final String URI ="http://purl.org/dc/elements/1.1/";
+    public static final String PREFIX ="dcterms";
+    private static final Model m = ModelFactory.createDefaultModel();
+    public static final Property rights = m.createProperty(URI, "rights");
+            
+            
+}
