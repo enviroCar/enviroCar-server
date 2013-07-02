3e4e87fb176586e7ce74ea500519d77615b95fa4
Jan Wirwahn <jan.wirwahn@wwu.de>
2013-07-02 13:32:57 +0200

    Added DUL and SSN ontologies

--- rest/src/main/java/org/envirocar/server/rest/encoding/rdf/linker/DUL.java
+++ rest/src/main/java/org/envirocar/server/rest/encoding/rdf/linker/DUL.java
@@ -4,10 +4,33 @@
  */
 package org.envirocar.server.rest.encoding.rdf.linker;
 
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.Resource;
+import static org.envirocar.server.rest.encoding.rdf.linker.SSN.URI;
+
 /**
  *
  * @author Jan Wirwahn
  */
 public class DUL {
+
+    public static final String URI = "http://www.loa-cnr.it/ontologies/DUL.owl#";
+    public static final String PREFIX = "dul";
+    private static final Model m = ModelFactory.createDefaultModel();
+    public static final Resource UnitOfMeasure =
+            m.createResource(URI + "UnitOfMeasure");
+    public static final Resource TimeInterval =
+            m.createResource(URI + "TimeInterval");
+    public static final Resource Amount =
+            m.createResource(URI + "Amount");
     
+    public static final Property isClassifiedBy =
+            m.createProperty(URI, "isClassifiedBy");
+    public static final Property hasDataValue =
+            m.createProperty(URI, "hasDataValue");
+
+    private DUL() {
+    }
 }
