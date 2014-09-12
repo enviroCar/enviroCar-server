d8930dd7437a2b70378ebb2f3c8a6180eb451a9b
matthesrieke <m.rieke@52north.org>
2014-09-12 11:01:03 +0200

    include GeodesicGeometryOperations

---
+++ core/src/main/java/org/envirocar/server/core/util/GeometryOperations.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2013 The enviroCar project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.envirocar.server.core.util;
+
+import java.util.List;
+
+import org.envirocar.server.core.entities.Measurement;
+
+public interface GeometryOperations {
+
+	public double calculateDistance(Measurement m1, Measurement m2);
+	
+	public double calculateLength(List<Measurement> measurements);
+	
+}
