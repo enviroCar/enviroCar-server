29f1e419a54f701e640ad084999a087c0db8d143
drifftr <drifftr@gmail.com>
2015-06-23 13:07:17 +0530

    added OSM rendering utils

--- rest/src/main/java/org/envirocar/server/rest/resources/ShareResource.java
+++ rest/src/main/java/org/envirocar/server/rest/resources/ShareResource.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2013 The enviroCar project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.envirocar.server.rest.resources;
 
 import java.awt.image.BufferedImage;
@@ -21,7 +37,6 @@ import org.envirocar.server.core.entities.Measurements;
 import org.envirocar.server.core.entities.Track;
 import org.envirocar.server.core.exception.TrackNotFoundException;
 import org.envirocar.server.core.filter.MeasurementFilter;
-import org.envirocar.server.core.statistics.Statistics;
 import org.envirocar.server.rest.MediaTypes;
 import org.envirocar.server.rest.util.OSMTileRenderer;
 import org.envirocar.server.rest.util.ShareImageRenderUtil;
@@ -53,8 +68,8 @@ public class ShareResource extends AbstractResource {
 			BufferedImage renderedImage = imp.process(image, hm.get(MAXSPEED),
 					hm.get(TIME), "0l/100km");
 			/*OSMTileRenderer osm=new OSMTileRenderer();
-			BufferedImage renderedImage = osm.getTile(getCoordinates(measurements));
-			*/byteArrayOS = new ByteArrayOutputStream();
+			BufferedImage renderedImage = osm.getTile(getCoordinates(measurements));*/
+			byteArrayOS = new ByteArrayOutputStream();
 			ImageIO.write(renderedImage, "png", byteArrayOS);
 
 		} catch (IOException e) {
@@ -68,7 +83,7 @@ public class ShareResource extends AbstractResource {
 	}
 	
 	@GET
-	@Path("map/"+TRACK)
+	@Path("preview/"+TRACK)
 	@Produces(MediaTypes.PNG_IMAGE)
 	public Response getMapImage(@PathParam("track") String trackId) {
 		ByteArrayOutputStream byteArrayOS = null;
@@ -216,7 +231,7 @@ public class ShareResource extends AbstractResource {
 	public ArrayList<Coordinate> getCoordinates(Measurements measurements){
 		ArrayList<Coordinate> coords=new ArrayList<Coordinate>();
 		for (Measurement m : measurements) {
-			coords.add(new Coordinate(m.getGeometry().getCoordinate().x ,m.getGeometry().getCoordinate().y)) ;
+			coords.add(m.getGeometry().getCoordinate()) ;
 		}
 		return coords;
 	}
--- rest/src/main/java/org/envirocar/server/rest/util/OSMTileRenderer.java
+++ rest/src/main/java/org/envirocar/server/rest/util/OSMTileRenderer.java
@@ -1,9 +1,29 @@
+/*
+ * Copyright (C) 2013 The enviroCar project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.envirocar.server.rest.util;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -14,48 +34,53 @@ import javax.imageio.ImageIO;
 import com.vividsolutions.jts.geom.Coordinate;
 
 public class OSMTileRenderer {
-	 int xTile = 0, yTile = 0;
+	final static String saveFileDir = "";
 
-	public  BufferedImage getTile(ArrayList<Coordinate> coords)
+	public BufferedImage getTile(ArrayList<Coordinate> coords)
 			throws IOException {
-		int zoom = 17;
+		int zoom = 19;
 		Coordinate coor = getLowestLat(coords);
 		double lat = coor.y;
 		double lon = coor.x;
+		zoom= getZoomLevel(coords);
 		// format to single tile
 		String picUri = "http://tile.openstreetmap.org/"
 				+ getTileNumber(lat, lon, zoom) + ".png";
 		System.out.println(picUri);
-		URL url;
-
-		url = new URL(picUri);
+		
+		URL url = new URL(picUri);
 		InputStream is = url.openStream();
-		BufferedImage image = ImageIO.read(is);
+		BufferedImage image = new BufferedImage(512, 256,BufferedImage.TYPE_INT_RGB);//ImageIO.read(is);
+		BufferedImage image1 = ImageIO.read(is);
 		Graphics2D g2d = image.createGraphics();
-		drawRoute(g2d, coords, zoom);
+		g2d.drawImage(image1,0,0,null);
+		g2d.drawImage(image1,256,0,null);
+		drawRoute(g2d, coords, zoom); 
 
 		g2d.dispose();
 		return image;
 	}
 
-	public  Coordinate getLowestLat(ArrayList<Coordinate> coords) {
+	public Coordinate getLowestLat(ArrayList<Coordinate> coords) {
 		double tempLat = 360.0;
 		Coordinate low = null;
 		for (Coordinate coord : coords) {
-			if (tempLat > coord.y) {
-				tempLat = coord.y;
+			if (tempLat > coord.x) {
+				tempLat = coord.x;
 				low = coord;
+				System.out.println(low);
 			}
 		}
+		System.out.println(low);
 		return low;
 	}
 
-	public  int getXTile(double lon, int zoom) {
+	public int getXTile(double lon, int zoom) {
 		int x = getInteger((lon + 180) / 360 * (1 << zoom));
 		return x;
 	}
 
-	public  int getYTile(double lat, int zoom) {
+	public int getYTile(double lat, int zoom) {
 		int y = getInteger((1 - Math.log(Math.tan(Math.toRadians(lat)) + 1
 				/ Math.cos(Math.toRadians(lat)))
 				/ Math.PI)
@@ -63,8 +88,8 @@ public class OSMTileRenderer {
 		return y;
 	}
 
-	public  Graphics2D drawRoute(Graphics2D g2d,
-			ArrayList<Coordinate> coords, int zoom) {
+	public Graphics2D drawRoute(Graphics2D g2d, ArrayList<Coordinate> coords,
+			int zoom) {
 		g2d.setPaint(new Color(4, 138, 191));
 		g2d.setStroke(new BasicStroke(3));
 		for (int i = 0; i < coords.size(); i++) {
@@ -73,35 +98,62 @@ public class OSMTileRenderer {
 				double oldY = coords.get(i).y;
 				double newX = coords.get(i + 1).x;
 				double newY = coords.get(i + 1).y;
-				if (xTile == getXTile(oldX, zoom) // check whether point is out of the bbox
-						&& xTile == getXTile(newX, zoom)) { //check for yTiles too, route last point and new point
-					/*System.out.println(i + "==> " + oldX + ":" + oldY + ":"
-							+ newX + ":" + newY + "===>"
-							+ getX(oldX, zoom, 256) + ":"
-							+ getY(oldY, zoom, 256) + ":"
-							+ getX(newX, zoom, 256) + ":"
-							+ getY(newY, zoom, 256));*/
-					g2d.drawLine(getX(oldX, zoom, 256), getY(oldY, zoom, 256),
-							getX(newX, zoom, 256), getY(newY, zoom, 256));
-				}
+				g2d.drawLine(getX(oldX, zoom, 256), getY(oldY, zoom, 256),
+						getX(newX, zoom, 256), getY(newY, zoom, 256));
+				System.out.println();
 			}
 		}
 		return g2d;
 	}
 
-	public  int getX(final double lon, final int zoom, int pic) {
-		double x = getFraction((lon + 180) / 360 * (1 << zoom));
-		System.out.print(x + " ++ ");
+	public int getX(final double lon, final int zoom, int pic) {
+		double x = getFraction((lon + 180) / 360 * (1 << zoom),34160); 
+		System.out.print(getXTile(lon, zoom) + " ++ ");
 		return (int) Math.round((x * pic));
 	}
 
-	public  int getY(final double lat, final int zoom, int pic) {
+	public int getY(final double lat, final int zoom, int pic) {
 		double y = getFraction((1 - Math.log(Math.tan(Math.toRadians(lat)) + 1
 				/ Math.cos(Math.toRadians(lat)))
 				/ Math.PI)
-				/ 2 * (1 << zoom));
-		//System.out.print(y + " ++ ");
+				/ 2 * (1 << zoom),21666);
+		 System.out.print(getYTile(lat, zoom) + " ++ ");
 		return (int) Math.round(y * pic);
+	}
+
+	public int getZoomLevel(ArrayList<Coordinate> coords) {
+		BoundingBox bbox = findBoundingBoxForGivenLocations(coords);
+		int leastZoomLevelX = 1;
+		int leastZoomLevelY = 1;
+		int baseTileY;
+		int baseTileX;
+		int finalZoom = 1;
+		for (int zoom = 19; zoom >= 1; zoom--) { // considering x
+			if ((getTileDetails(bbox.east, bbox.north, zoom)[1] - getTileDetails(
+					bbox.west, bbox.north, zoom)[1]) <= 0) {
+				leastZoomLevelX = zoom;
+				break;
+			} else {
+				leastZoomLevelX = 0; // special case
+			}
+		}
+		for (int zoom = 19; zoom >= 1; zoom--) {// considering y
+			if ((getTileDetails(bbox.west, bbox.north, zoom)[0] - getTileDetails(
+					bbox.west, bbox.south, zoom)[0]) <= 0) {
+				leastZoomLevelY = zoom;
+				break;
+			} else {
+				leastZoomLevelY = 0; // special case
+			}
+		}
+		if (leastZoomLevelY > leastZoomLevelX) {
+			finalZoom = leastZoomLevelX;
+		} else if (leastZoomLevelX > leastZoomLevelY) {
+			finalZoom = leastZoomLevelY;
+		} else {
+			finalZoom = leastZoomLevelX;
+		}
+		return finalZoom;
 	}
 
 	public int getLastXTile(final double lastLon, final int zoom, int pic) {
@@ -113,11 +165,11 @@ public class OSMTileRenderer {
 		return numAdd;
 	}
 
-	public double getFraction(double num) {
+	public double getFraction(double num,int baseTileValue) {
 
 		long iPart = (long) num;
-		//System.out.print(num + " -- ");
-		double fPart = num - iPart;
+		// System.out.print(num + " -- ");
+		double fPart = num - baseTileValue; // num-baseTileNumFOr X or Y(36785)
 		return fPart;
 	}
 
@@ -126,7 +178,7 @@ public class OSMTileRenderer {
 		int iPart = (int) num;
 		return iPart;
 	}
- 
+
 	public String getTileNumber(final double lat, final double lon,
 			final int zoom) {
 		int xtile = (int) Math.floor((lon + 180) / 360 * (1 << zoom));
@@ -143,9 +195,28 @@ public class OSMTileRenderer {
 			ytile = 0;
 		if (ytile >= (1 << zoom))
 			ytile = ((1 << zoom) - 1);
-		xTile = xtile;
-		yTile = ytile;
+
 		return ("" + zoom + "/" + xtile + "/" + ytile);
+	}
+
+	public int[] getTileDetails(final double lat, final double lon,
+			final int zoom) {
+		int xtile = (int) Math.floor((lon + 180) / 360 * (1 << zoom));
+		int ytile = (int) Math
+				.floor((1 - Math.log(Math.tan(Math.toRadians(lat)) + 1
+						/ Math.cos(Math.toRadians(lat)))
+						/ Math.PI)
+						/ 2 * (1 << zoom));
+		if (xtile < 0)
+			xtile = 0;
+		if (xtile >= (1 << zoom))
+			xtile = ((1 << zoom) - 1);
+		if (ytile < 0)
+			ytile = 0;
+		if (ytile >= (1 << zoom))
+			ytile = ((1 << zoom) - 1);
+		System.out.println("" + zoom + "/" + xtile + "/" + ytile);
+		return new int[] { xtile, ytile };
 	}
 
 	BoundingBox tile2boundingBox(final int x, final int y, final int zoom) {
@@ -157,13 +228,81 @@ public class OSMTileRenderer {
 		return bb;
 	}
 
-	 double tile2lon(int x, int z) {
+	double tile2lon(int x, int z) {
 		return x / Math.pow(2.0, z) * 360.0 - 180;
 	}
 
-	 double tile2lat(int y, int z) {
+	double tile2lat(int y, int z) {
 		double n = Math.PI - (2.0 * Math.PI * y) / Math.pow(2.0, z);
 		return Math.toDegrees(Math.atan(Math.sinh(n)));
+	}
+
+	public BoundingBox findBoundingBoxForGivenLocations(
+			ArrayList<Coordinate> coordinates) {
+		double west = 0.0;
+		double east = 0.0;
+		double north = 0.0;
+		double south = 0.0;
+
+		for (int lc = 0; lc < coordinates.size(); lc++) {
+			Coordinate loc = coordinates.get(lc);
+			if (lc == 0) {
+				north = loc.y;
+				south = loc.y;
+				west = loc.x;
+				east = loc.x;
+			} else {
+				if (loc.y > north) {
+					north = loc.y;
+				} else if (loc.y < south) {
+					south = loc.y;
+				}
+				if (loc.x < west) {
+					west = loc.x;
+				} else if (loc.x > east) {
+					east = loc.x;
+				}
+			}
+		}
+
+		return new BoundingBox(north, south, east, west);
+	}
+
+	public void saveFile(BufferedImage image, String trackId)
+			throws IOException {
+		String filePath = saveFileDir + "/" + trackId + ".png";
+		File f = new File(filePath);
+		if (!f.exists()) {
+			ImageIO.write(image, "PNG", new File(filePath));
+		}
+	}
+
+	public Graphics2D appendImage(BufferedImage newTile,int highestX,int lowestX,int highestY,int lowestY,int zoom)
+			throws IOException {
+		Graphics2D g2d = newTile.createGraphics();
+		for (int i = lowestX; i <= highestX; i++) {
+			Graphics2D temp;
+			for (int j = lowestY; j <= highestY; j++) {
+				BufferedImage image=downloadTile(i, j, zoom);
+				//temp.add(0,256,image.createGraphics());
+				//temp=composite image
+			}
+			//g2d.add(256*(i-lowestX),0,temp);
+		}
+		return g2d;
+	}
+	
+	public BufferedImage downloadTile(int x,int y,int zoom) throws IOException {
+		String picUri = "http://tile.openstreetmap.org/"
+				 + zoom + "/" + x + "/" + y+ ".png";
+		URL url = new URL(picUri);
+		InputStream is = url.openStream();
+		BufferedImage image = ImageIO.read(is);
+		return image;
+	}
+
+	public void loadFile(String trackId) throws IOException {
+		// if exists load image as a BI else download and create new BI
 	}
 }
 
@@ -172,4 +311,24 @@ class BoundingBox {
 	double south;
 	double east;
 	double west;
+
+	public BoundingBox() {
+
+	}
+
+	public BoundingBox(double north, double south, double east, double west) {
+		super();
+		this.north = north;
+		this.south = south;
+		this.east = east;
+		this.west = west;
+	}
+
+}
+
+class OSMTile {
+	int xValue;
+	int yValue;
+	BufferedImage image;
+
 }
