78e569949ede5cb8637ce20107a86efdb049cf64
matthesrieke <m.rieke@52north.org>
2015-08-07 14:33:58 +0200

    custom LeakPreventor impl

---
+++ webapp/src/main/java/org/envirocar/server/EnvirocarLeakPreventor.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2013 The enviroCar project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.envirocar.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import se.jiderhamn.classloader.leak.prevention.ClassLoaderLeakPreventor;
+
+/**
+ *
+ * @author matthes
+ */
+public class EnvirocarLeakPreventor extends ClassLoaderLeakPreventor {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(EnvirocarLeakPreventor.class);
+
+    @Override
+    protected void error(Throwable t) {
+        LOGGER.warn("Exception on shutting down resource.", t);
+    }
+    
+}
